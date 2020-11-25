3e6cb18ce98b16adaf59b7457249cbd7a0a49b22
Luca Ventrice <luca@ventrice.ch>
2020-11-25 17:47:14 +0100

    add new task list view

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.apiOverview, name="api-overview"),
+	path('', views.api_overview, name="api-overview"),
+	path('task-list/', views.task_list, name="task-list"),
 ]
